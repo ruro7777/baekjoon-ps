be191ab7c6a0228f972d2e41f60e9c2c411d3cae
ruro7777 <157967192+ruro7777@users.noreply.github.com>
2025-05-06 17:18:15 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.cc
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int arr[3][3];
+
+int N;
+
+
+int dfs(int n, int m)
+{
+  if (arr[n][m] == 0) return 0;
+  if (n==N-1 && m==N-1) return 1;
+  if (n>=N || m>=N) return 0;
+
+  return max(dfs(n+arr[n][m], m), dfs(n, m+arr[n][m]));
+}
+
+int main(void)
+{
+    scanf("%d", &N);
+    for (int i = 0; i<N; i++)
+    {
+        for (int j = 0; j<N; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    if (dfs(0,0)) 
+        printf("HaruHaru");
+    else
+        printf("Hing");
+}
